a05de1ddcf4297a88d5e2581cd7fb16f04fe6b97
C5198513 <C5198513@PVGD50817110A.apj.global.corp.sap>
2014-08-20 11:18:53 +0800

    commit by yule.ma ModelManager

--- WebContent/model/ModelManager.js
+++ WebContent/model/ModelManager.js
@@ -1,9 +1,12 @@
-jQuery.sap.declare("poc.fiori.wechat.model.ModelManager");
+jQuery.sap.declare("model.ModelManager");
 
-ModelManager.getModelUrlPre = function(){
+model.ModelManager = {};
+model.ModelManager.getModelUrlPre = function(){
 	return "http://localhost:8080/poc.fiori.wechat/proxy/http/10.59.145.101:9001/";
+	//182.254.156.24:8000
 };
 
-ModelManager.getPicUrlPre = function(){
-	return "http://10.59.145.101:9001/";
+model.ModelManager.getPicUrlPre = function(){
+	return "http://10.59.145.101:9001";
+	//182.254.156.24:9001
 };
